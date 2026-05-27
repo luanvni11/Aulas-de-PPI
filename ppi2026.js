2edba4715d886f8d04492b89140bc6496203d933
luanvni11 <luannivicius@gmail.com>
2026-05-27 18:50:55 +0000

    desafio PPI

--- ppi2026.js
+++ ppi2026.js
@@ -35,79 +35,98 @@ console.log("5" === 5); // Compara tipo E valor
 console.log(x++); // incremento após o retorno do valor
 console.log(x);
 console.log(++x); // incremento antes do retorno do valor
+
+// x++;
+// ++x;
+// x += 1;
+// x = x + 1;
+
+// Operadores lógicos - && (AND), || (OR), ! (NOT)
 let isExpression = (true && 5 + 2 * 3 < 10) || false;
 console.log(isExpression);
 
-//if...else
-//if (condition) {
-//condition = verdadeira
+// if (condition) {
+//   // condition === true
+// } else {
+//   // condition === false
+// }
 
-//} else {
-//condition = falsa
+// Template Strings
+let text = `React é 'tudo de bom!'\\
+"Programar" é minha vida!/
+Eu amo o IFRN`;
+console.log(text);
 
-//}
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
 
-//template strings
-let labubu = `amo programar
-em javascript
-é muito legal!`;
-console.log(labubu);
+// Array
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-//for (let index=0; index<Array.length; index++) {
-//    const element = array[index];
-//}
+console.log(fruits.length); // Tamanho
 
-//array
-let fruits = ["banana", "orange", "apple", "mango"];
+console.log(fruits[0]); // Posição 0
 
-console.log(fruits.lenght); // tamanho
-
-console.log(fruits[0]);
-
-fruits.push("kiwi");
+// Insere elemento no final da lista
+fruits.push("Kiwi");
 console.log(fruits);
 
-console.log(fruits.pop()); // remove o último elemento
+// Remove e retorna o último elemento
+console.log(fruits.pop());
 console.log(fruits);
 
-console.log(fruits.shift()); // remove o primeiro elemento
+// Remove e retorna o primeiro elemento
+console.log(fruits.shift());
 console.log(fruits);
 
-fruits.unshift("lemon"); // adiciona um elemento no início do array
+// Insere elemento no começo da lista
+fruits.unshift("Lemon");
 console.log(fruits);
 
-fruits.splice(2, 0, "kiwi", "blueberry"); // argumento 1 - posição, argumento 2 - quantidade de elementos a remover, argumento 3 - elemento a adicionar
+// Argumento 1 - posição
+// Argumento 2 - qts elementos serão removidos
+// Argumento 3 - lista de elementos que serão inseridos
+fruits.splice(2, 0, "Kiwi", "Blueberry");
 console.log(fruits);
 
 fruits.splice(2, 1);
 console.log(fruits);
 
-fruits.splice(3, 1, "kiwi");
+// Desafio - substituir 'Apple' por 'Kiwi'
+fruits.splice(3, 1, "Kiwi");
 console.log(fruits);
 
-let index = fruits.indexOf("kiwi");
+// Busca índice do elemento 'Kiwi'
+let index = fruits.indexOf("Kiwi");
 console.log(index);
 
-fruits.splice(index, 1, "apple");
+fruits.splice(index, 1, "Apple");
 console.log(fruits);
 
-console.log("sort/reverse");
-fruits.sort(); // ordena o array em ordem alfabética
-console.log(fruits);
-
-//ordem decrscente
-fruits.reverse();
-console.log(fruits);
-
-let fruitsSort = fruits.toSorted();
+console.log("Sort/Reverse");
+let fruitsSort = fruits.toSorted(); // Não altera array
 console.log(fruits);
 console.log(fruitsSort);
 
-let fruitsReverse = fruits.toReversed();
+let fruitsReverse = fruits.toReversed(); // Não altera array
 console.log(fruits);
 console.log(fruitsReverse);
 
+// Ordem crescente
+fruits.sort();
+console.log(fruits);
+
+// Ordem decrescente
+fruits.reverse();
+console.log(fruits);
+
 const numbers = [45, 4, 9, 16, 25];
+console.log(numbers);
+
+// DESAFIO
+// Criar um array 'numbers2' que armazene os valores de 'numbers' * 2
+// Output: [90, 8, 18, 32, 50]
 const numbers2 = [];
 for (let i = 0; i < numbers.length; i++) {
   numbers2.push(numbers[i] * 2);
@@ -123,9 +142,160 @@ const numbersMap = numbers.map(myFunction);
 console.log(numbersMap);
 
 console.log(numbers.map((number) => number * 2));
-console.log(numbers.toSorted((a, b) => a - b));
-console.log(numbers.toSorted((a, b) => b - a));
+
+console.log(numbers.toSorted((a, b) => a - b)); //C
+console.log(numbers.toSorted((a, b) => b - a)); //D
 
 numbers.sort((a, b) => a - b);
-console.log('maior =', numbers[numbers.length - 1]); // maior número
-console.log(numbers[0]); // menor número
+// Maior valor
+console.log("Maior =", numbers[numbers.length - 1]);
+// Menor valor
+console.log("Menor =", numbers[0]);
+
+// **********************************
+// Aula 13/05 - Funções, array destructuring, spread operator
+console.log(fruits);
+
+const fruits2 = ["Kiwi", "Avocado"];
+console.log([...fruits2, "Grape"]);
+
+// Spread - ...
+const fruits3 = [...fruits, ...fruits2];
+console.log(fruits3);
+
+// DESAFIO - Exibir todas as frutas que comecem
+// com a letra A
+const out = [];
+const letra = "a";
+
+// for (let i = 0; i < fruits3.length; i++) {
+//   const fruit = fruits3[i];
+//   //toLowerCase - minúscula, toUpperCase - maiúscula
+//   if (fruit[0].toLowerCase() === letra) {
+//     out.push(fruit);
+//   }
+// }
+
+// fruits3.map((fruit) => {
+//   if (fruit[0].toLowerCase() === letra) {
+//     out.push(fruit);
+//   }
+// });
+
+// find - retorna a primeira ocorrência,
+// de acordo com a condição
+let outFind = fruits3.find((fruit) => fruit[0].toLowerCase() === letra);
+console.log(outFind);
+
+// filter - retorna TODAS as ocorrências,
+// de acordo com a condição
+let outFilter = fruits3.filter((fruit) => fruit[0].toLowerCase() === letra);
+console.log(outFilter);
+
+// DESAFIO - Exibir o valor da soma de todos os
+// números do array 'numbers'
+
+console.log(numbers);
+
+// map
+let soma = 0;
+numbers.map((number) => {
+  soma += number;
+});
+console.log(soma);
+
+// reduce
+console.log(numbers.reduce((soma, number) => soma + number));
+
+// Object
+const pessoa = {
+  nome: "Zé Vaqueiro",
+  idade: 25,
+  profissao: "Cantor/Compositor",
+};
+
+console.log(pessoa.nome);
+console.log(pessoa["nome"]);
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+
+const pessoa2 = { ...pessoa, hobby: "Vaquejada" };
+console.log(pessoa2);
+
+// Atribuição por desestruturação
+const { nome, idade } = pessoa2;
+console.log(nome, idade);
+
+// Construtor - Protótipo de Objeto
+function Product(name, sto, ram, price, qty, category) {
+  this.name = name;
+  this.sto = sto;
+  this.ram = ram;
+  this.price = price;
+  this.qty = qty;
+  this.category = category;
+}
+
+const p1 = new Product("iPhone", 256, 16, 5000, 10, "Celular");
+const p2 = new Product("iPad", 512, 16, 8000, 20, "Tablet");
+const p3 = new Product("Macbook", 1024, 24, 15000, 30, "Notebook");
+const p4 = new Product("iPhone Pro Max", 256, 16, 7000, 15, "Celular");
+const p5 = new Product("iPad Pro", 512, 16, 10000, 25, "Tablet");
+const p6 = new Product("Macbook Pro", 1024, 24, 25000, 35, "Notebook");
+
+const estoque = [
+  { ...p1 },
+  { ...p2 },
+  { ...p3 },
+  { ...p4 },
+  { ...p5 },
+  { ...p6 },
+];
+console.log(estoque);
+
+/* DESAFIO 1
+
+  a) Valor total de todos os produtos em estoque
+  b) Ordene os produtos por nome (crescente/descrescente)
+  c) Ordene os produtos por preço (crescente/descrescente)
+  d) Filtre produtos de acordo uma categoria
+
+*/
+
+console.log("--- DESAFIO 1 ---");
+
+// a)
+const valorTotal = estoque
+  .reduce((soma, product) => soma + product.price * product.qty, 0)
+  .toFixed(2);
+
+console.log(`Valor total: R$ ${valorTotal}`);
+
+// b)
+const nomes = estoque.map((product) => product.name);
+
+const nomesCrescente = nomes.toSorted();
+console.log("Nomes em ordem crescente:");
+console.log(nomesCrescente);
+
+const nomesDecrescente = nomesCrescente.toReversed();
+console.log("Nomes em ordem decrescente:");
+console.log(nomesDecrescente);
+
+// c)
+const precosCrescente = estoque.toSorted((a, b) => a.price - b.price);
+console.log("Produtos por preço (crescente):");
+console.log(precosCrescente);
+
+const precosDecrescente = estoque.toSorted((a, b) => b.price - a.price);
+console.log("Produtos por preço (decrescente):");
+console.log(precosDecrescente);
+
+// d)
+const categoriaDesejada = "Tablet";
+const produtosFiltrados = estoque.filter(
+  (product) => product.category === categoriaDesejada,
+);
+
+console.log(`Produtos da categoria: ${categoriaDesejada}`);
+console.log(produtosFiltrados);
